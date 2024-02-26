36a35bbc6c157191c5a0032141b36c543ec65e87
abilanravi <97493499+abilanravi@users.noreply.github.com>
2024-02-26 15:18:03 -0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portfolio
+        My Portfolio
       </a>
       <div className={styles.menu}>
         <img
